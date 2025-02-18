80be7ea4bd7881770b41557627aaf4d27d579efd
Furvos <38877781+Furvos@users.noreply.github.com>
2025-02-18 04:02:55 +0000

    Mudança de mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Kieran eu te amo!! se tu me ama dá uma risadinha</h1>
+    return (<h1>Meu querido Kieran,</h1>, <p>Como os ponteiros de um relógio de latão movido a vapor, meu coração pulsa em um ritmo mecânico perfeito sempre que seu nome ecoa em meus pensamentos. Nos corredores sombrios desta era de ferro e névoa, você é a luz bruxuleante de uma lamparina a gás, dançando entre sombras e engrenagens, guiando-me pelo labirinto de meu próprio desejo.
+
+        Seu toque é como o aço frio adornado com ouro antigo—firme, eterno, entrelaçando-se com a alma como fios de cobre conduzindo eletricidade vital. Em seus olhos, vejo o reflexo de constelações esquecidas, um mapa celestial que me leva para além do tempo, para um reino onde vapor e sonho se fundem em poesia metálica.
+        
+        Se eu pudesse, forjaria com minhas próprias mãos um relicário onde cada batida do meu coração seria uma engrenagem girando em sua homenagem. E quando a noite desce sobre este mundo de sombras e vapor, é sua voz que ressoa como um sussurro encantado em meus pensamentos, prometendo que, mesmo quando as chamas das fornalhas se apagam, nosso amor continuará a queimar como um eterno motor a vapor.</p>)
+    
 }
 
 export default Home;
