2b8dc01f5110e2c35caa56ef53ce0d839953b61a
Furvos <38877781+Furvos@users.noreply.github.com>
2025-02-18 04:08:57 +0000

    Mudança de mensagem 2.2

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 function Home() {
     return (
-        <>
+        <React.Fragment>
             <h1>Hello</h1>
             <p>Welcome to my website</p>
-        </>
+        </React.Fragment>
     );
-}
-    
 }
 
 export default Home;
